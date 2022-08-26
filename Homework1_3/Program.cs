8601778d4d4c65af972e69bbbb6d87eaddc65404
insalt <maximthebeast99@gmail.com>
2022-08-26 15:15:41 +0400

    HomeworkFirstTask

--- Homework1_3/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.WriteLine("Введите число");
-int num = int.Parse(Console.ReadLine());
-
-if (num%2 == 0) Console.WriteLine($"Число {num} является четным");
-else Console.WriteLine($"Число {num} является нечетным");
--- Homework1_4/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.WriteLine("Введите число");
-int num = int.Parse(Console.ReadLine());
-
-int i = 2;
-while (i <= num)
-{
-    Console.Write($"{i} ");
-    i = i+2;
-}
