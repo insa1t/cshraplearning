7e01dc6c2b8e0d5ab13518c6ffabdaa7ee988310
insalt <maximthebeast99@gmail.com>
2022-09-26 20:31:54 +0400

    Homework8_68

---
+++ Homework8_68/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите два положительных числа: M и N.");
+int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+Console.WriteLine($"A({m}, {n}) = {Func(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int Func(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Func(m - 1, 1);
+    else
+        return Func(m - 1, Func(m, n - 1));
+}
